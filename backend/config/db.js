6c38f109c0cc51b9cf780caa317f22843b9936fc
Mohamed <uodauomohamed@gmail.com>
2024-06-13 16:48:10 -0700

    "Updated db.js to use environment variables for local and atlas database connections"

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,10 @@
 const mongoose=require('mongoose');
 require('dotenv').config();
 
-const db=process.env.DATABASE_URL
+const localDB = process.env.DATABASE_LOCAL;
+const atlasDB = process.env.DATABASE_ATLAS;
+
+const db = process.env.NODE_ENV === 'production' ? atlasDB : localDB;
 
 mongoose.connect(db)
     .then(()=>console.log("Connected to MongoDB"))
