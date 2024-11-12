4188d1e552aeb93c0e14a1e89794fca984e09168
Mohamed ouadou <uoaduomohamed@gmail.com>
2024-11-12 07:50:06 -0800

    Update CORS origin to use environment variables

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:5173'
+  origin: process.env.NODE_ENV === 'production' ? process.env.CLIENT_URL : process.env.FRONTEND_BASE_URL
 }));
 
 
